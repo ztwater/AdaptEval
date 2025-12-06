471478d1e1da62a185a9fb0da444fae59e95966d
ztwater <2442626596@qq.com>
2025-12-06 11:17:16 +0800

    highlight the path issue in globals.py

--- evaluation/src/globals.py
+++ evaluation/src/globals.py
@@ -1,7 +1,7 @@
 import os
 
 class Globals:
-    ROOT_PATH = os.path.abspath("../../")   # your absolute root path
+    ROOT_PATH = os.path.abspath("path/to/root")   # your absolute root path
     DATASET_PATH = os.path.join(ROOT_PATH, "benchmark")
     METADATA_PATH = os.path.join(ROOT_PATH, "metadata.json")
     RESULT_PATH = os.path.join(ROOT_PATH, "evaluation", "results")
